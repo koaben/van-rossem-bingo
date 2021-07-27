7a5793951545120e312e105810f0ce780023723c
Thijs Kuilman <thijs.kuilman@gmail.com>
2021-07-27 20:12:38 +0200

    Updated styling

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
    
+
+   <div style="position: relative; display: flex; justify-content: center; margin-top: 50px">
+    <img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_nologo/5567033/5567033-1591538095363-bfa84457378f7.jpg" style="height: 100px; width: 100px; position: absolute; border: 2px solid #fff; border-radius: 5px;">
+   </div>
 <div class="main-content">
   <div class="title"> 
-
-
     <span>
-
-<!-- 
-<img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_nologo/5567033/5567033-1591538095363-bfa84457378f7.jpg" style="width: px;"> -->
-
     Van Rossem Bingo  
-    </span></div>
-  <div class="bingo-card">
-
-    <BingoItem :key="item" v-for="item in items" :msg="item"/>
-
-    </div>
+    </span>
   </div>
+  <div class="bingo-card" style="  justify-content: center;">
+    <BingoItem :key="item" v-for="item in items" :msg="item"/>
+  </div>
+</div>
 </div>
 </template>
 
@@ -33,7 +30,7 @@ this.items = (this.items).sort(() => 0.5 - Math.random());
     return {
       items: [
        "In allerlei opzichten",
-       "Een stelletje halve garen",
+       "Lampje van de camera",
        "Niet goed snik",
        "In feite",
        "Volslagen idioten",
@@ -125,12 +122,12 @@ footer a .icons {
 }
 
 .title {
-  background: #fdb90b;
-  color: #fff;
+  color: #ffec5e;
   padding: 30px 10px;
+  padding-top: 65px;
   grid-column: span 4;
   text-align: center;
-  font: 72px/0.9 "Amatic SC", cursive;
+  font-size: 36px;
 }
 .title span {
   display: none;
@@ -141,16 +138,15 @@ footer a .icons {
 
 
 .bingo-card {
-  background: #f7d75c;
   padding: 10px;
   display: grid;
   grid-gap: 3px;
   grid-template-rows: repeat(4, 110px);
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 25%);
   text-transform: uppercase;
 }
 .bingo-card__item {
-  background: #f9f59e;
+  background: rgba(0,0,0,0.5);
   display: flex;
   align-items: center;
   text-align: center;
@@ -160,6 +156,7 @@ footer a .icons {
   font-size: 12px;
   line-height: 1.35;
   user-select: none;
+  color: #fff;
 }
 .bingo-card__item:after {
   content: "-";
@@ -175,10 +172,24 @@ footer a .icons {
   font: 280px/0.5 "Caveat Brush", cursive;
   text-align: center;
   transform: rotate(-45deg);
+   -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 .bingo-card__item.active:after {
   height: 100%;
   opacity: 0.7;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 
 .bingo-card__item {
@@ -209,6 +220,18 @@ footer a .icons {
 }
 .clear-button:hover {
   color: #f2ae00;
+}
+
+.main-content {
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+background-color: #5f6eba;
+background-image: url("https://www.transparenttextures.com/patterns/dark-dotted-2.png");
+/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}
+
+html, body {
+  background: #121731;
 }
 
 </style>
