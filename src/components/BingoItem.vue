6c6cbe376992dbac03a7df4dab2ed0350a5b0d45
Thijs Kuilman <thijs.kuilman@gmail.com>
2021-07-30 22:53:02 +0200

    Fixed mobile click

--- src/components/BingoItem.vue
+++ src/components/BingoItem.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="bingo-card__item" @click="toggleActive">
-
-    <textarea v-if="editMode" v-model="quote" style="width: 98%;" rows="4" @input="quoteChanged"></textarea>
-    <span v-else>
+  <main>
+    <div class="bingo-card__item" style="height:100%; width: 100%" @click="toggleActive" v-if="!editMode">
       {{ quote }}
-    </span>
-    <span class="bingo-card__checkbox"></span>
-  </div>
+      <span class="bingo-card__checkbox"></span>
+    </div>
+
+    <div class="bingo-card__item" style="height:100%; width: 100%" @click="toggleActive" v-if="editMode">
+      <textarea v-model="quote" style="width: 98%;" rows="4" @input="quoteChanged"></textarea>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -24,7 +26,7 @@ export default {
   },
   methods: {
     toggleActive(event) {
-      if(!this.editMode) {
+      if (!this.editMode) {
         event.target.classList.toggle('active')
       }
     },
